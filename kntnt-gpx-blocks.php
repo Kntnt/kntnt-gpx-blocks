e75ce5caf02b8031d5462d7507ccd88681dead85
Thomas Barregren <thomas@kntnt.com>
2026-05-07 19:48:34 +0200

    Bump version to 0.1.0 for the initial release

--- kntnt-gpx-blocks.php
+++ kntnt-gpx-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Kntnt GPX Blocks
  * Plugin URI:        https://github.com/Kntnt/kntnt-gpx-blocks
  * Description:       Gutenberg blocks for visualising GPX tracks: map, elevation profile, and statistics.
- * Version:           1.0.0
+ * Version:           0.1.0
  * Requires at least: 6.5
  * Requires PHP:      8.4
  * Author:            Kntnt
